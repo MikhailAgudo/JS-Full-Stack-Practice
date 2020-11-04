262529cd87ef037a2971d9882f18605dff45c96e
Seraguith <mikhail.agudo@gmail.com>
2020-11-04 16:23:10 +0800

    Finished palindromes exercise.

--- javascript-exercises/palindromes/palindromes.js
+++ javascript-exercises/palindromes/palindromes.js
@@ -1,5 +1,45 @@
-const palindromes = function() {
+function checkLegality(char) {
+    illegalChar = ["!", " ", ",", "."];
 
+    for(let i = 0; i < illegalChar.length; i++) {
+        if (char === illegalChar[i]) {
+            return false;
+        }
+    }
+    return true;
+
+    // This will not work, because the return only returns
+    // on the function inside the forEach() method; not
+    // the checkLegality() method.
+    //
+    //illegalChar.forEach(function(item) {
+    //    console.log(`checking ${char} and ${item}`);
+    //    if (char === item) {
+    //        return false;
+    //        console.log("get!");
+    //    }
+    //});
+}
+
+const palindromes = function(string) {
+    let word = "";
+    for (let i = 0; i < string.length; i++) {
+        let char = string.charAt(i);
+        if (checkLegality(char) === true) {
+            word += char.toUpperCase();
+        }
+    }
+    
+    let reversedWord = "";
+    for (let i = (word.length - 1); i >= 0; i--) {
+        reversedWord += word[i];
+    }
+
+    if (word === reversedWord) {
+        return true;
+    } else {
+        return false;
+    }
 }
 
 module.exports = palindromes
