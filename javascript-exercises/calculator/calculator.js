624a1c646bd44fa67ea0b7204a3fafddf2755d73
Seraguith <mikhail.agudo@gmail.com>
2020-11-04 15:53:25 +0800

    Added my solutions for the calculator exercise.

--- javascript-exercises/calculator/calculator.js
+++ javascript-exercises/calculator/calculator.js
@@ -1,25 +1,45 @@
-function add () {
-	
+function add (input1, input2) {
+	return input1 + input2;
 }
 
-function subtract () {
-	
+function subtract (input1, input2) {
+	return input1 - input2;
 }
 
-function sum () {
-	
+function sum (numArray) {
+	let sum = 0;
+	for (let i = 0; i < numArray.length; i++) {
+		sum += numArray[i];
+	}
+	return sum;
 }
 
-function multiply () {
-	
+function multiply (numArray) {
+	let result = 1;
+	for (let i = 0; i < numArray.length; i++) {
+		result *= numArray[i];
+	}
+	return result;
 }
 
-function power() {
-	
+function power(input, power) {
+	let result = input;
+	for (let i = 1; i < power; i++) {
+		result *= input;
+	}
+	return result;
 }
 
-function factorial() {
-	
+function factorial(input) {
+	if (input === 0) {
+		return 1;
+	} else {
+		let result = 1;
+		for (let i = 0; i < input; i++) {
+			result *= (i + 1);
+		}
+		return result;
+	}
 }
 
 module.exports = {
