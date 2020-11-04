458f64a75d8913c81356188c70e0592d9726326a
Seraguith <mikhail.agudo@gmail.com>
2020-11-04 17:11:24 +0800

    Finished the fibonacci exercise.

--- javascript-exercises/fibonacci/fibonacci.js
+++ javascript-exercises/fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if (position < 1) {
+        return "OOPS";
+    }
 
+    let sum = 0;
+    let sequence = [0, 1];
+
+    for (let i = 1; i < position; i++) {
+        sequence.push(sequence[1] + sequence.shift());
+        //sum += sequence.shift();
+        //sequence.push(sum);
+        console.log(sequence[1]);
+    }
+    return sequence[1];
 }
 
 module.exports = fibonacci
