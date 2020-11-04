b2b74dd7c0ec95919ecdf3d357f5f52faabddbe0
Seraguith <mikhail.agudo@gmail.com>
2020-11-04 17:22:03 +0800

    Finished findTheOldest exercise.

--- javascript-exercises/findTheOldest/findTheOldest.js
+++ javascript-exercises/findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-let findTheOldest = function() {
+let findTheOldest = function(people) {
+    let CURRENT_YEAR = 2020;
+    let oldestIndex = 0;
+    let oldestAge = 0;
+    for (let i = 0; i < people.length; i++) {
+        if (people[i].yearOfDeath === undefined) {
+            let age = CURRENT_YEAR - people[i].yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestIndex = i;
+            }
+        } else {
+            let age = people[i].yearOfDeath - people[i].yearOfBirth;
+            if (age > oldestAge) {
+                oldestAge = age;
+                oldestIndex = i;
+            }
+        }
+    }
 
+    return people[oldestIndex];
 }
 
 module.exports = findTheOldest
