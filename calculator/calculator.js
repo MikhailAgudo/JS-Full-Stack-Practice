cd549f1a418c876d7668641178bc4211bc76a7f9
Seraguith <mikhail.agudo@gmail.com>
2020-11-06 09:29:14 +0800

    Added comments for the calculations.

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -44,11 +44,17 @@ class calculator {
 
     calculate(string) {
         // Where all the calculation starts and happens.
-        // First, determineSteps() then calculate everything
-        // one by one.
+        // firstFilter() turns the input string into
+        // a readable input, which secondFilter() then
+        // calculates. Check for errors in between the
+        // filters, because firstFilter() has created
+        // a readable input.
 
         let screenArray = this.firstFilter(string);
 
+        // if checkError() returns something meaningful
+        // execute the code. checkError() only returns
+        // if there's an actual error.
         if (this.checkError(screenArray)) {
             screenArray = this.checkError(screenArray);
             this.screenText = screenArray;
@@ -57,66 +63,110 @@ class calculator {
 
         screenArray = this.secondFilter(screenArray);
 
+        // Then, modify screenText to contain the answer.
+        // Return it, for unit test purposes.
         this.screenText = screenArray[0];
 
         return screenArray[0];
     }
 
     firstFilter(string) {
-        // Determine what steps are needed to calculate
-        // via PEMDAS/MDAS and an array containing a
-        // dictionary of "operation", "input1", "input2"
-        // and "result".
-        // This method will also take care of reading
-        // the screen.
-        // The other inputs within the array will contain
-        // an index within the same array that uses
-        // the "result" variable.
-
+        // Initialize screenArray, which will be used
+        // throughout the calculation and error-checking
+        // process.
         let screenArray = [];
 
+        // i serves as a starting point for slicing the
+        // the string, while j is the end point.
+        // The for loop allows us to append values
+        // onto the screenArray, for reading of later
+        // functions. 
         let i = 0;
         for (let j = 0; j < string.length; j+=0) {
+            // checkedChar lets us evaluate a true/false
+            // value from checkOperators(), making
+            // checking if it's an operator much simpler.
             let checkedChar = this.checkOperators(string[j]);
+
+            // If it's an operator, slice the string from
+            // i to j; which will happen from the start of
+            // a number to the end. And because isEnd is
+            // false, also slice j to j+1 which is the
+            // operator. sliceScreen() does all of that
+            // in one function.
             if (checkedChar === true) {
                 screenArray = this.sliceScreen(string, 
                     screenArray, i, j, false);
+                
+                // The main reason why the counter is
+                // j+=0 is because of this. Add j++ directly
+                // which makes j go from the start of the
+                // operator ("|+") to the end ("+|").
+                // Then set i as that, so the next slice
+                // starts at the number, not the operator.
                 j++;
                 i = j;
             } else if (j === (string.length - 1)) {
+                // At the end, slice from i to the
+                // end of the string. isEnd is true because
+                // we do not want to slice a non-existing
+                // operator.
                 screenArray = this.sliceScreen(string, 
                     screenArray, i, string.length, true);
+
                 j++;
             } else if (checkedChar === false) {
+                // Otherwise, just increment j to move on.
                 j++;
             }
         }
+        // removeDuds() is important, because of how
+        // the above algorithm works it will sometimes
+        // add "" to screenArray. This mostly occurs
+        // with double operators. It prevents any
+        // unnecessary NaN from happening.
         screenArray = this.removeDuds(screenArray);
+
         return screenArray;
     }
 
     checkOperators(char) {
+        // Loop around OPERATORS to see if char
+        // is one of them. This function is used by
+        // firstFilter() and many other error-checking
+        // functions.
         for (let i = 0; i < this.OPERATORS.length; i++) {
             if (char === this.OPERATORS[i]) {
                 return true;
             }
         }
+
         return false;
     }
 
     removeDuds(screenArray) {
+        // As explained in firstFilter(), removeDuds()
+        // removes "" for us, created by the firstFilter()
+        // algorithm. It's a work-around, and truthfully
+        // firstFilter() should be written better, but
+        // this works.
         for (let i = 0; i < screenArray.length; i++) {
             if (screenArray[i] === "") {
                 screenArray.splice(i, 1);
+
+                // We want to decrement because splice
+                // removes an element. If we don't, the
+                // loop will skip an element.
                 i--;
             }
         }
+
         return screenArray;
     }
 
     secondFilter(screenArray) {
-        // Add the Ms, then Ds, then As, then Ss. Then
-        // splice them. The replaced value should be the calculation
+        // Calculate screenArray, from left-to-right
+        // and doing MD first then AS in MDAS.
 
         this.MDFilter(screenArray);
         this.ASFilter(screenArray);
@@ -125,62 +175,97 @@ class calculator {
     }
 
     MDFilter(screenArray) {
+        // Loop, and check if the element is an operator.
+        // If it is, do an operation with the element
+        // before it and after it. Afterwards, splice
+        // with the result. Set i = 0 to do the
+        // left-to-right thing in MDAS, properly.
+        // We don't check if it's dividing by zero here
+        // because error checking has already happened
+        // after screenArray was created by firstFilter().
         for (let i = 0; i < screenArray.length; i+=0) {
             if (screenArray[i] === this.OPERATORS[0]) {
                 let input1 = parseFloat(screenArray[i - 1]);
                 let input2 = parseFloat(screenArray[i + 1]);
                 let result = this.multiply(input1, input2);
+
                 screenArray.splice((i - 1), 3, result);
+
                 i = 0;
             } else if (screenArray[i] === this.OPERATORS[1]) { 
                 let input1 = parseFloat(screenArray[i - 1]);
                 let input2 = parseFloat(screenArray[i + 1]);
                 let result = this.divide(input1, input2);
+
                 screenArray.splice((i - 1), 3, result);
+
                 i = 0;
             } else {
+                // If nothing happened, increment.
+                // Seems like useless code. Just i++
+                // the loop and remove this.
                 i++;
             }
         }
+
         return screenArray;
     }
 
     ASFilter(screenArray) {
+        // Same as MDFilter() but with addition and 
+        // subtraction.
         for (let i = 0; i < screenArray.length; i+=0) {
             if (screenArray[i] === this.OPERATORS[2]) {
                 let input1 = parseFloat(screenArray[i - 1]);
                 let input2 = parseFloat(screenArray[i + 1]);
                 let result = this.add(input1, input2);
+
                 screenArray.splice((i - 1), 3, result);
+
                 i = 0;
             } else if (screenArray[i] === this.OPERATORS[3]) {
                 let input1 = parseFloat(screenArray[i - 1]);
                 let input2 = parseFloat(screenArray[i + 1]);
                 let result = this.subtract(input1, input2);
+
                 screenArray.splice((i - 1), 3, result);
+
                 i = 0;
             } else {
                 i++;
             }
         }
+
         return screenArray;
     }
 
-    sliceScreen(string, firstFilter, i, j, isEnd) {
-        // This method reads the string to help
-        // determineSteps().
+    sliceScreen(string, screenArray, i, j, isEnd) {
+        // Slices the string and appends it to
+        // screenArray.
 
+        // filterInput is the slice from i to j, which
+        // is usually the number.
         let filterInput = string.slice(i, j);
+        screenArray.push(filterInput);
 
-        firstFilter.push(filterInput);
-
+        // If it's not at the end of the string,
+        // also slice the operator via j to j+1.
         if (isEnd === false) {
-            firstFilter.push(string.slice(j, j + 1));
+            screenArray.push(string.slice(j, j + 1));
         }
-        return firstFilter;
+
+        return screenArray;
     }
 
     checkError(screenArray) {
+        // Contains all of the error checking in
+        // one function so we don't have to call
+        // all of them each time. Also makes it cleaner.
+
+        // Check if it's a string, because if it is
+        // that means screenArray has already encountered
+        // an error in a previous error-check. If it isn't
+        // a string, do all of the error checking.
         if (typeof screenArray != "string"){
             screenArray = this.checkDivideZero(screenArray);
             screenArray = this.checkDoubleOperator(screenArray);
@@ -188,6 +273,9 @@ class calculator {
             screenArray = this.checkMultipleDecimals(screenArray);
         }
         
+        // Check if it's already one of the errors.
+        // If it is, return it, which will trigger
+        // "if (this.checkError(screenArray)) {}"
         for (let i = 0; i < this.ERRORS.length; i++) {
             if (screenArray === this.ERRORS[i]) {
                 return screenArray;
@@ -196,16 +284,25 @@ class calculator {
     }
 
     checkDivideZero(screenArray) {
+        // Check if it's 0 and "/", which is
+        // dividing by zero. Simplifies the error
+        // checking for zero.
         for (let i = 0; i < screenArray.length; i++) {
             if (screenArray[i] == 0 && 
                 screenArray[i - 1] === this.OPERATORS[1]) {
                 return this.ERROR_DIVIDE_BY_ZERO;
             }
         }
+
         return screenArray;
     }
 
     checkDoubleOperator(screenArray) {
+        // Check if something is a double operator
+        // like "+-". isOperator lets us see if there's
+        // already a previous operator before it. Otherwise
+        // if the current element is not an operator then
+        // make isOperator false.
         let isOperator = false;
         for (let i = 0; i < screenArray.length; i++) {
             if (this.checkOperators(screenArray[i]) === true && isOperator === true){
@@ -216,10 +313,14 @@ class calculator {
                 isOperator = false;
             }
         }
+
         return screenArray;
     }
 
     checkEndingOperator(screenArray) {
+        // Check the first and final elements if they're
+        // operators. This checks if the string has
+        // incomplete input with operators.
         let endIndex = screenArray.length - 1
         if (this.checkOperators(screenArray[endIndex]) === true) {
             return this.ERROR_SYNTAX;
@@ -227,13 +328,28 @@ class calculator {
         if (this.checkOperators(screenArray[0]) === true) {
             return this.ERROR_SYNTAX;
         }
+
         return screenArray;
     }
 
     checkMultipleDecimals(screenArray) {
+        // Check if a number has multiple decimals.
+        // On the for loop with j, we check the entire
+        // element. If there has already been a decimal,
+        // make hasDecimal = true. After the loop, when
+        // we're done with the element, make
+        // hasDecimal = false. This is important because
+        // we only need one decimal.
+        //
+        // In an earlier version of the function,
+        // it used to turn hasDecimal = false inside
+        // the loop. This is a no-no because that means
+        // the loop resets hasDecimal immediately after
+        // checking the character.
         let hasDecimal = false;
         for (let i = 0; i < screenArray.length; i++) {
             let input = String(screenArray[i]);
+
             for (let j = 0; j < input.length; j++) {
                 if (input[j] === "." && hasDecimal === true) {
                     return this.ERROR_SYNTAX
@@ -241,8 +357,10 @@ class calculator {
                     hasDecimal = true;
                 }
             }
+
             hasDecimal = false;
         }
+
         return screenArray;
     }
 
@@ -264,6 +382,13 @@ class calculator {
 }
 
 function unitTest(calc) {
+    // unitTest() lets us test the calculator to see
+    // if it's actually correct with its functions.
+    // Though this could have been written better,
+    // it was made with haste and this works anyway.
+    // unitTest will never be touched again, now that
+    // all the intended functions have been implemented.
+
     if (calc.calculate("6*5-3/4+2") === (6 * 5 - 3 / 4 + 2)) {
         // Test MDAS.
         console.log("First test: Pass");
