6b98feeaa2929d3e023d0a97ea5d4df7b4aa648f
Seraguith <mikhail.agudo@gmail.com>
2020-11-17 13:07:26 +0800

    Added some minor comments and starting code.

---
+++ library/library.js
@@ -0,0 +1,11 @@
+// A class for the UI and makes books for $myLibrary.
+
+let myLibrary = [];
+
+function Book() {
+    // The constructor...
+}
+
+function addBookToLibrary() {
+    // Do stuff here
+}
