7672c885bdf8c18fe6f7819fcc2411eeb83a824d
Seraguith <mikhail.agudo@gmail.com>
2020-11-25 11:31:12 +0800

    Added back-end functions. Finished Book class. Added a very simple unit test.

--- library/library.js
+++ library/library.js
@@ -2,10 +2,33 @@
 
 let myLibrary = [];
 
-function Book() {
+function Book(author, title, pageCount, hasRead) {
     // The constructor...
+    this.author = String(author);
+    this.title = String(title);
+    this.pageCount = parseInt(pageCount);
+    this.hasRead = hasRead;
 }
 
-function addBookToLibrary() {
+function addBookToLibrary(author, title, pageCount, hasRead) {
     // Do stuff here
+    const newBook = new Book(author, title, pageCount, hasRead);
+    myLibrary.push(newBook);
 }
+
+function changeReadStatus(index) {
+    let targetBook = myLibrary[index];
+    if (targetBook.hasRead === true) {
+        targetBook.hasRead = false;
+    } else {
+        targetBook.hasRead = true;
+    }
+}
+
+function unitTest() {
+    addBookToLibrary("Andrej Sapkowski", "Season of Storms", 415, false);
+    addBookToLibrary("Tales of the Dying Earth", "Jack Vance", 300.0, true);
+
+}
+
+unitTest();
