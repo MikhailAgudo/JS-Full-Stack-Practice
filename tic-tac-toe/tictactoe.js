b106645361883e0add4bb81958b0472812e9ad17
Seraguith <mikhail.agudo@gmail.com>
2020-12-03 16:38:30 +0800

    Added placing stuff on tiles. Added match simulations in unitTest. Added resetBoard() for resetting everything back.

--- tic-tac-toe/tictactoe.js
+++ tic-tac-toe/tictactoe.js
@@ -1,26 +1,49 @@
 const gameBoard = (() => {
-    let board = [];
+    const EMPTY_BOARD = [0,0,0,0,0,0,0,0,0];
+    let board = [0,0,0,0,0,0,0,0,0];
+    let turnStatus = 1;
     let victoryStatus = 0;
-
-    for (let i = 0; i < 9; i++) {
-        board.push(0);
-    }
 
     const returnVictory = () => {
         return victoryStatus;
-    }
+    };
 
     const returnBoard = () => {
         return board;
-    }
+    };
 
     const replaceBoard = (inputBoard) => {
         board = inputBoard;
-    }
+    };
 
     const overrideVictory = (inputVictory) => {
         victoryStatus = inputVictory;
+    };
+
+    const resetBoard = () => {
+        console.log(`REPLACING ${board} with ${EMPTY_BOARD}`);
+        board = [0,0,0,0,0,0,0,0,0];
+        turnStatus = 1;
+        victoryStatus = 0;
     }
+
+    const changeTurn = () => {
+        if (turnStatus === 1) {
+            turnStatus = 2;
+        } else {
+            turnStatus = 1;
+        }
+    }
+
+    const placeOnTile = (index) => {
+        if (victoryStatus === 0 && board[index] === 0) {
+            board[index] = turnStatus;
+            changeTurn();
+            checkVictory();
+        } else {
+            console.log("ILLEGAL MOVE");
+        }
+    };
 
     const checkPattern = (start, end, increment, checkIncrement) => {
         for (let i = start; i < end; i += increment) {
@@ -54,17 +77,30 @@ const gameBoard = (() => {
     return {
         replaceBoard,
         overrideVictory,
+        placeOnTile,
         returnVictory,
         returnBoard,
-        checkVictory
+        checkVictory,
+        resetBoard
     };
 })();
 
 const unitTest = (() => {
+    const simulateMatch = (turnHistory, output) => {
+        gameBoard.resetBoard();
+        console.log(gameBoard.returnBoard());
+        for (let i = 0; i < turnHistory.length; i+=0) {
+            console.log(turnHistory[0]);
+            gameBoard.placeOnTile(turnHistory.shift());
+        }
+
+        testVictory(gameBoard.returnBoard(), output);
+    }
+
     const testVictory = (board, output) => {
         gameBoard.replaceBoard(board);
         gameBoard.overrideVictory(0);
-        //console.log(gameBoard.returnBoard());
+        console.log(gameBoard.returnBoard());
         gameBoard.checkVictory();
 
         let boardOutput = gameBoard.returnVictory();
@@ -77,6 +113,7 @@ const unitTest = (() => {
     }
 
     return {
+        simulateMatch,
         testVictory
     };
 })();
@@ -90,3 +127,14 @@ unitTest.testVictory([0,0,0,0,0,0,1,1,1], 1);
 unitTest.testVictory([0,1,0,0,0,1,0,1,0], 0);
 unitTest.testVictory([1,0,0,0,1,0,0,0,1], 1);
 unitTest.testVictory([0,0,1,0,1,0,1,0,0], 1);
+unitTest.testVictory([2,0,0,2,0,0,2,0,0], 2);
+unitTest.testVictory([0,2,0,0,2,0,0,2,0], 2);
+unitTest.testVictory([0,0,2,0,0,2,0,0,2], 2);
+unitTest.testVictory([2,2,2,0,0,0,0,0,0], 2);
+unitTest.testVictory([0,0,0,2,2,2,0,0,0], 2);
+unitTest.testVictory([0,0,0,0,0,0,2,2,2], 2);
+unitTest.testVictory([2,0,0,0,2,0,0,0,2], 2);
+unitTest.testVictory([0,0,2,0,2,0,2,0,0], 2);
+
+unitTest.simulateMatch([4,8,3,2,5],1);
+unitTest.simulateMatch([7,6,4,1,0,8,5,3],0)
