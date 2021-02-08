fa6516790f3cb7f9aaa0f2361aa85adb87fdb590
MikhailAgudo <mikhail.agudo@gmail.com>
2021-02-08 16:12:58 +0800

    Tasks now only have descriptions. Some functions have been optimized. Added some UI for tasks.

--- quest-journal/src/ui/taskRenderer.js
+++ quest-journal/src/ui/taskRenderer.js
@@ -5,38 +5,38 @@ const taskRenderer = (() => {
     const render = (task, displayedQuest, i) => {
         // early version of render()
         let newTask = document.createElement("div");
-
-        let taskTitle = document.createElement("div");
-        taskTitle.textContent = task.getTitle();
-
-        let taskDescription = document.createElement("div");
-        taskDescription.textContent = task.getDescription();
-
-        let taskDueDate = document.createElement("div");
-        taskDueDate.textContent = task.getDueDate();
-
-        let taskPriority = document.createElement("div");
-        taskPriority.textContent = task.getPriority();
+        newTask.classList.add("task-container");
+        let taskDescription = createTaskArea(task.getDescription());
 
         let deleteButton = createDeleteButton(displayedQuest, i);
 
-        newTask.appendChild(taskTitle);
+        //newTask.appendChild(taskTitle);
+        newTask.appendChild(deleteButton);
         newTask.appendChild(taskDescription);
         //newTask.appendChild(taskDueDate);
         //newTask.appendChild(taskPriority);
-        newTask.appendChild(deleteButton);
 
         return newTask;
     }
+
     const createDeleteButton = (displayedQuest, i) => {
         let deleteButton = document.createElement("button");
-        deleteButton.textContent = "del";
+        deleteButton.textContent = "Done";
         deleteButton.addEventListener("click", (e) => {
             questStructurer.removeTask(displayedQuest, i);
             questRenderer.renderTaskTab();
         });
 
         return deleteButton;
+    }
+
+    const createTaskArea = (textContent) => {
+        let newTaskArea = document.createElement("div");
+        newTaskArea.classList.add("task-area");
+
+        newTaskArea.textContent = textContent;
+
+        return newTaskArea;
     }
 
     return {
